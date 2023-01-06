eecb3de185a302164af24ab813c35925f67b9eee
Seth DImmer <sethdimmer1@gmail.com>
2023-01-06 18:45:00 -0500

    initial commit

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -2,8 +2,14 @@ import React from "react";
 
 const Featured = () => {
     return (
-        <section>
-            Featured
+        <section id="features">
+            <div className="container">
+                <div className="row">
+                    <h2 className="section__title">
+                        Featured <span className="purple">Books</span>
+                    </h2>
+                </div>
+            </div>
         </section>
     )
 }
