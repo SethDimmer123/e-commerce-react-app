1dc3a88372fee5641546d3bdac1910de31928375
Seth DImmer <sethdimmer1@gmail.com>
2023-01-07 12:27:57 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import Nav from "./components/Nav.jsx";
 import Home from "./pages/Home";
 import Footer from './components/Footer';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Books from './pages/Books.jsx';
 
 
 function App() {
   return (
     <Router>
-    <div className="App">
+    <div className="app">
       <Nav />
-      <Route />
-      <Home />
+      <Route path="/" exact component={Home} />
+      <Route path="/books" exact component={Books} />
       <Footer />
     </div>
     </Router>
