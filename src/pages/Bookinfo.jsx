77978bc01292a36fd7b70228a8792a25db93123b
Seth DImmer <sethdimmer1@gmail.com>
2023-01-08 13:01:33 -0500

    initial commit

--- src/pages/Bookinfo.jsx
+++ src/pages/Bookinfo.jsx
@@ -72,7 +72,7 @@ const BookInfo = ({ books }) => {
                         </div>
                         <div className="books">
                         {
-                            books.filter(book => book.rating === 5 && book.id !== id )
+                            books.filter(book => book.rating === 5 && +book.id !== +id )
                             .slice(0, 4)
                             .map(book => <Book book={book} key={book.id} />)
                         }
