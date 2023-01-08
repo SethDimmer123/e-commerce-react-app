0b641cdcef550aa528decb74aa37693379388ec1
Seth DImmer <sethdimmer1@gmail.com>
2023-01-08 14:05:13 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import './App.css'
 import Landing from './components/Landing.jsx'
+import React, {useEffect, useState} from "react";
 import Nav from "./components/Nav.jsx";
 import Home from "./pages/Home";
 import Footer from './components/Footer';
@@ -7,16 +8,33 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Books from './pages/Books.jsx';
 import { books } from "./data";
 import BookInfo from './pages/Bookinfo.jsx';
+import Cart from './pages/Cart.jsx';
+
 
 
 function App() {
+  const [cart,setCart] = useState([]);
+
+  function addToCart(book) {
+    setCart([...cart, {...book, quantity:1}])
+
+  }
+
+  useEffect(() => {
+    console.log(cart);
+  }, [cart])
+
+
+
+
   return (
     <Router>
     <div className="App">
       <Nav />
       <Route path="/" exact component={Home} />
       <Route path="/books" exact render={() => <Books books={books}/>} />
-      <Route path="/books/:id" render={() => <BookInfo books={books}/>} />
+      <Route path="/books/:id" render={() => <BookInfo books={books} addToCart={addToCart}/>} />
+      <Route path="/cart" render={() => <Cart books={books}/>} />
       <Footer />
     </div>
     </Router>
