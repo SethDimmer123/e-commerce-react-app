caa9ecf27ecba27d6dbee293e98301a2cae191cc
Seth DImmer <sethdimmer1@gmail.com>
2023-01-08 05:41:50 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Landing from './components/Landing.jsx'
 import Nav from "./components/Nav.jsx";
 import Home from "./pages/Home";
 import Footer from './components/Footer';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Books from './pages/Books.jsx';
 import { books } from "./data";
 import BookInfo from './pages/Bookinfo.jsx';
@@ -12,11 +12,11 @@ import BookInfo from './pages/Bookinfo.jsx';
 function App() {
   return (
     <Router>
-    <div className="app">
+    <div className="App">
       <Nav />
       <Route path="/" exact component={Home} />
-      <Route path="/books"  render={() =><Books books={books}/>} />
-      <Route path="/books/1" render={() =><BookInfo books={books}/>} />
+      <Route path="/books"  render={() => <Books books={books}/>} />
+      <Route path="/books/1" render={() => <BookInfo books={books}/>} />
       <Footer />
     </div>
     </Router>
