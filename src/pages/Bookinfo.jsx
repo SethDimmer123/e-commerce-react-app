b60fc00f498e0ef9910a29b87d84281458ecc637
Seth DImmer <sethdimmer1@gmail.com>
2023-01-08 12:10:30 -0500

    initial commit

--- src/pages/Bookinfo.jsx
+++ src/pages/Bookinfo.jsx
@@ -1,6 +1,8 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Link } from "react-router-dom";
+import Rating from "../components/ui/Rating";
+import Price from "../components/ui/Price";
 
 const BookInfo = ({ books }) => {
     return (
@@ -12,6 +14,22 @@ const BookInfo = ({ books }) => {
                             <Link to="/books" className="book__link">
                                 <FontAwesomeIcon icon="arrow-left" />
                             </Link>
+                            <Link to="/books" className="book__link">
+                                <h2 className="book__selected--title--top">Books</h2>
+                            </Link>
+                        </div>
+                        <div className="book__selected">
+                            <figure className="book__selected--figure">
+                                <img src="https://covers.openlibrary.org/b/id/8091016-L.jpg" alt="" className="book__selected--ig"></img>
+                            </figure>
+                            <div className="book__selected--description">
+                                <h2 className="book__selected--title">Crack the coding interview</h2>
+                                <Rating rating="4.5" />
+                                <div className="book__selected--price">
+                                    <Price originalPrice={50} salePrice={20} />
+                                </div>
+                                <div className="book__summary"></div>
+                            </div>
                         </div>
                     </div>
                 </div>
